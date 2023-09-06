03b953d4b4cfdbe9b28d88c3f24a7a09b66bf3aa
RicardoCM77 <124858910+RicardoCM77@users.noreply.github.com>
2023-09-06 16:55:03 +0000

    ⭐Enhance:App Add author route to users router

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/author', function(_, res) {
+  res.render('author', {author: "Ricardo Cortes"});
+});
+module.exports = router;
