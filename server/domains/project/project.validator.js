98426302e09a7e7c6ad174e6756772400d01e42b
BuzzSmash96 <142968793+BuzzSmash96@users.noreply.github.com>
2023-11-15 16:37:57 +0000

    🦖REQUIRED☄️

--- server/domains/project/project.validator.js
+++ server/domains/project/project.validator.js
@@ -5,7 +5,7 @@ const projectSchema = Yup.object().shape({
   name: Yup.string().required('se requiere nombre del proyecto'),
   description: Yup.string()
     .max('la descripción debe no tener mas de 500 caracteres ')
-    .required('se requiere ua descripcion de proyecto'),
+    .required('se requiere una descripcion de proyecto'),
 });
 
 // Middleware de extracción
